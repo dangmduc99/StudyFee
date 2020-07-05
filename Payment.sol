3173ede90d80758ec13bc341c70c536d9d50370e
dangmduc99 <dangmduc99@gmail.com>
2020-07-05 16:53:59 +0700

    push

--- Payment.sol
+++ Payment.sol
@@ -2,56 +2,120 @@
 pragma solidity ^0.6.9;
 pragma experimental ABIEncoderV2;
 
-contract UserManager {
+contract Manager {
     struct Student {
-        string name;
         string studentId;
+        string studentName;
         string password;
         bool isStudent;
+        string addr;
+        string publicKey;
+        string privateKey;
+    }
+
+    struct Subject {
+        string subjectId;
+        string subject;
+        string num;
+    }
+
+    struct Class {
+        string classId;
+        string subjectId;
+        string classRoom;
     }
 
     struct Transaction {
         string transactionId;
         string studentId;
-        uint schoolYear;
-        uint studyFee;
-        string state;
-        string dateStart;
-        string dueDate;
-        string paidDate;
+        string receiver;
+        string amount;
+        string datetime;
     }
 
-    struct School {
-        string userName;
-        string password;
-        string addr;
+    struct StudyFee {
+        // string Id;
+        string studentId;
+        string studyFee;
+        string dueDate;
     }
 
 
     mapping (string => Student) public students;
+    mapping (string => Subject) public subjects;
+    mapping (string => Class) public classes;
     mapping (string => Transaction) public transactions;
-    mapping (string => School) public schools;
+    mapping (string => StudyFee) public studyFees;
 
-    function signUp(string memory studentId,
-                    string memory _name,
+
+    //for sign up
+    function createStudent (string memory _studentId,
+                    string memory _studentName,
                     string memory _password) public {
-        students[studentId] = Student(studentId, _name, _password, true);
+        students[_studentId] = Student(_studentId, _studentName, _password, true, '', '', '');
     }
 
-    function signIn(string memory studentId, string memory _password) public view returns (Student memory) {
+
+    //for sign in
+    function getStudent (string memory studentId) public view returns (Student memory) {
         Student storage student = students[studentId];
-        if (student.password == _password) {
-            return student;
-        } else {
-            
-        }
+        return student;
     }
 
-    function updateStudent(string memory std_id,
-                        string memory _name,
+    //for update profile
+    function updateStudent (string memory studentId,
+                        string memory _studentName,
                         string memory _password) public {
-        Student storage student = students[std_id];
-        student.name = _name;
+        Student storage student = students[studentId];
+        student.studentName = _studentName;
         student.password = _password;
+    }
+
+    //for create subject
+    function createSubject (string memory _subjectId,
+                        string memory _subject,
+                        string memory _num) public {
+        subjects[_subjectId] = Subject(_subjectId, _subject, _num);
+    }
+
+    //for edit subject
+    function editSubject (string memory subjectId,
+                        string memory _subject,
+                        string memory _num) public {
+        Subject storage subject = subjects[subjectId];
+        subject.subject = _subject;
+        subject.num = _num;
+    }
+
+    //for create class
+    function createClass (string memory _classId,
+                        string memory _subjectId,
+                        string memory _classRoom) public {
+        classes[_classId] = Class(_classId, _subjectId, _classRoom);
+    }
+
+    //for edit class
+    function editClass (string memory classId,
+                        string memory _subjectId,
+                        string memory _classRoom) public {
+        Class storage class = classes[classId];
+        class.subjectId = _subjectId;
+        class.classRoom = _classRoom;
+    }
+
+    //for register class
+    function registerClass (string memory _studentId,
+                        string memory _studyFee,
+                        string memory _dueDate) public {
+        studyFees[_studentId] = StudyFee(_studentId, _studyFee, _dueDate);
+    }
+
+    //for save transaction information
+    function transaction (string memory _transactionId,
+                        string memory _studentId,
+                        string memory _receiver,
+                        string memory _amount,
+                        string memory _dateTime) public {
+        transactions[_transactionId] = Transaction(_transactionId, _studentId, _receiver, _amount, _dateTime);
     }
 }
